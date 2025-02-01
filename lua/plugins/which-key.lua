8707e3793ada8c22a806d77a118c9bf9f1147b05
Justin Gifford <49419781+justgif@users.noreply.github.com>
2025-02-01 11:40:04 -0500

    Added keymap to show all keymaps with which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,12 +1,23 @@
 return {
     'folke/which-key.nvim',
+    event = 'VeryLazy',
     opts = {
         delay = 0,
         spec = {
+            { 's',          group = '[S]urround' },
             { '<leader>e',  group = '[E]xplore' },
             { '<leader>h',  group = '[H]arpoon' },
             { '<leader>hr', group = '[H]arpoon [R]eplace' },
             { '<leader>s',  group = '[S]earch' },
         },
     },
+    keys = {
+        {
+            '<leader>?',
+            function()
+                require('which-key').show({ global = true })
+            end,
+            desc = 'Buffer Local Keymaps'
+        }
+    }
 }
